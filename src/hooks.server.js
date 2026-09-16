1f510b718d5a06b989a727c823ae08fec3ed64ca
Barks-and-byter <jabarke2@our.ecu.edu.au>
2026-09-16 14:42:27 +0800

    fix: allow root path to bypass redirection in authorisation hook

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -7,7 +7,7 @@ import { initDB } from './utils/initDb';
 /** @type {import('@sveltejs/kit').Handle} */
 async function authorisation({ event, resolve }){
     //allow for my auth route to bypass redirection
-    if (event.url.pathname.startsWith('/auth') || event.url.pathname === '/verify') {
+    if (event.url.pathname.startsWith('/auth') || event.url.pathname === '/verify' || event.url.pathname === '/') {
         return await resolve(event);
     }
 
